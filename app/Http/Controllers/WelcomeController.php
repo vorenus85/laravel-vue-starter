ac83fb62bba8aa6850b3fe92ce2b6fd51fe0bc6d
Janos Perge <janos.perge@gmail.com>
2025-12-14 20:21:40 +0100

    fix WelcomePage name

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -8,6 +8,6 @@ class WelcomeController extends Controller
 {
     public function index()
     {
-        return Inertia::render('Welcome');
+        return Inertia::render('WelcomePage');
     }
 }
