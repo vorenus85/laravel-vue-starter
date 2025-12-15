6d07ee5b228df50ba89881b7b71fb04276deaa4a
Janos Perge <janos.perge@gmail.com>
2025-12-15 14:30:07 +0100

    add primevue

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,8 @@
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
 
 const pinia = createPinia()
 
@@ -15,6 +17,11 @@ createInertiaApp({
         createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(pinia)
+            .use(PrimeVue, {
+                theme: {
+                    preset: Aura,
+                },
+            })
             .mount(el)
     },
 })
